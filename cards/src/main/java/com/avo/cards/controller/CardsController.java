41c340137b25e3fd4f67a6c0a5ea65c40ef91d7b
jayadurga-t <jayadurga.t332@gmail.com>
2026-03-02 22:11:10 +0530

    feat(cards): implement resilience4j retry with fallback support

--- cards/src/main/java/com/avo/cards/controller/CardsController.java
+++ cards/src/main/java/com/avo/cards/controller/CardsController.java
@@ -6,6 +6,7 @@ import com.avo.cards.dto.CardsDto;
 import com.avo.cards.dto.ErrorResponseDto;
 import com.avo.cards.dto.ResponseDto;
 import com.avo.cards.service.ICardsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -202,11 +203,21 @@ public class CardsController {
             )
     }
     )
+    @Retry(name = "getBuildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo() {
+        logger.debug("getBuildInfo() method Invoked");
+        throw new NullPointerException();
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("getBuildInfoFallback() method Invoked");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(buildVersion);
+                .body("0.5");
     }
 
     @Operation(
